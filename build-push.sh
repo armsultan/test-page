e8f13289365003776fe52e146fed945db11b0f0d
Armand <armand@Armands-MacBook-Pro.local>
2022-02-14 09:14:57 -0700

    fixed script

--- build-push.sh
+++ build-push.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Log into Docekr first
+docker login
+
 # PLAIN TEXT PAGE
 docker build --no-cache -t test-page:plain-text -f DockerfilePlainText .
 docker build --no-cache -t test-page:plain-text-nonroot -f DockerfilePlainTextNonRoot .
@@ -32,24 +35,45 @@ docker build --no-cache -t test-page:planets-nonroot -f DockerfilePlanetsNonRoot
 docker build --no-cache -t test-page:snapt -f DockerfileSnapt .
 docker build --no-cache -t test-page:snapt-nonroot -f DockerfileSnaptNonRoot .
 
-## Push to your own Docker Hub
+## Tage and Push to your own Docker Hub
 docker tag test-page:plain-text armsultan/test-page:plain-text
+docker push armsultan/test-page:plain-text
+
 docker tag test-page:plain-text-nonroot armsultan/test-page:plain-text-nonroot
+docker push armsultan/test-page:plain-text-nonroot
 
 docker tag test-page:json armsultan/test-page:json
+docker push armsultan/test-page:json
+
 docker tag test-page:json-nonroot armsultan/test-page:json-nonroot
+docker push armsultan/test-page:json-nonroot
 
 docker tag test-page:html armsultan/test-page:html
+docker push armsultan/test-page:html
+
 docker tag test-page:html-nonroot armsultan/test-page:html-nonroot
+docker push armsultan/test-page:html-nonroot
 
 docker tag test-page:blue armsultan/test-page:blue
+docker push armsultan/test-page:blue
+
 docker tag test-page:blue-nonroot armsultan/test-page:blue-nonroot
+docker push armsultan/test-page:blue-nonroot
 
 docker tag test-page:green armsultan/test-page:green
+docker push armsultan/test-page:green
+
 docker tag test-page:green-nonroot armsultan/test-page:green-nonroot
+docker push armsultan/test-page:green-nonroot
 
 docker tag test-page:planets armsultan/test-page:planets
+docker push armsultan/test-page:planets
+
 docker tag test-page:planets-nonroot armsultan/test-page:planets-nonroot
+docker push armsultan/test-page:planets-nonroot
 
 docker tag test-page:snapt armsultan/test-page:snapt
+docker push armsultan/test-page:snapt
+
 docker tag test-page:snapt-nonroot armsultan/test-page:snapt-nonroot
+docker push armsultan/test-page:snapt-nonroot
